f90a15f9b52d8693cbeb68741b02d0a714a1e846
joacundo2 <facundogo.garavagliaolivera@gmail.com>
2026-05-14 19:45:53 +0200

    :qw

--- backend-extracted/Backend-Travel-Agency-dev/src/main/java/com/inditex/g1_agencia_viajes/exception/MinorWithoutTutorException.java
+++
@@ -1,7 +0,0 @@
-package com.inditex.g1_agencia_viajes.exception;
-
-public class MinorWithoutTutorException extends RuntimeException {
-    public MinorWithoutTutorException() {
-        super("Un menor de edad debe ir acompañado de un tutor para crear la reserva");
-    }
-}
